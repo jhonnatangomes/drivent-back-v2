dc7e642e483130f3f6dfd0a665b8bc842100c7ef
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-05-24 20:51:27 -0300

    solved bug

--- src/setup.ts
+++ src/setup.ts
@@ -1,5 +1,10 @@
 import dotenv from 'dotenv';
 
-const path = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+const path =
+  process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : process.env.NODE_ENV === 'test'
+    ? '.env.test'
+    : '.env.development';
 
 dotenv.config({ path });
